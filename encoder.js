3ddc5e7ca8ee3ba4808c55540edf50acdab609be
simonfan <simonxd@gmail.com>
2013-07-03 18:45:50 -0300

    encoder first commit

---
+++ encoder.js
@@ -0,0 +1,54 @@
+define(['buildable','underscore'], function(Buildable, undef) {
+
+	var Encoder = Object.create(Buildable);
+
+	Encoder.extend({
+		dictionaries: {},
+
+		init: function(dictionaries) {
+			this.dictionaries = _.extend(this.dictionaries, dictionaries);
+		},
+
+		define: function(name, dictionary) {
+
+			if (typeof name === 'object') {
+				var _this = this;
+				_.each(name, function(dictionary, name) {
+					_this.define(name, dictionary);
+				});
+
+			} else {
+				this.dictionaries[ name ] = dictionary;
+			}
+		},
+
+		encode: function(dictionary, str) {
+			dictionary = this.dictionaries[ dictionary ];
+
+			console.log(dictionary);
+
+			if (dictionary) {
+				_.each(dictionary.ini, function(ini, index) {
+					str = str.replace(ini, dictionary.res[ index ]);
+				});
+			}
+
+			return str;
+		},
+
+		decode: function(dictionary, str) {
+			dictionary = this.dictionaries[ dictionary ];
+
+			if (dictionary) {
+				_.each(dictionary.ini, function(ini, index) {
+					str = str.replace(dictionary.res[ index ], ini);
+				});
+			}
+
+			return str;
+		}
+	});
+
+
+	return Encoder;
+});
---
+++ tests/demo.js
@@ -0,0 +1,25 @@
+define(['jquery','encoder'], function($, Encoder) {
+
+
+	var encoder = Encoder.build({
+		'spaced-title': {
+			ini: ['-', ' '],
+			res: ['_-_', '-']
+		}
+	});
+
+
+	$('#e-input').change(function(e) {
+		var $target = $(e.target),
+			value = encoder.encode('spaced-title', $target.val());
+
+		$('#d-input').val(value);
+	});
+
+	$('#d-input').change(function(e) {
+		var $target = $(e.target),
+			value = encoder.decode('spaced-title', $target.val());
+
+		$('#e-input').val(value);
+	});
+});
